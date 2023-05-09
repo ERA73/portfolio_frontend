2835367a91ed90fd7b396cc06b17f499b45a37d0
era73 <era-73@gmail.com>
2023-05-09 16:13:49 -0500

    implement send contact message and manage response

--- client/src/api/contact.api.js
+++ client/src/api/contact.api.js
@@ -1,5 +1,22 @@
 import axios from 'axios'
 
-export const getAllContacts = () => {
-    return axios.get('http://localhost/contact/api/v1/contact/')
-}
+const contactApi = axios.create({
+    baseURL: 'http://localhost/contact/api/v1/contact/'
+})
+
+// export const getAllContacts = () => {
+//     return contactApi.get('/')
+// }
+// export const sendContact = (contact) => {
+//     return contactApi.post('/', contact)
+// }
+
+export const getAllContacts = () =>  contactApi.get('/');
+
+export const sendContact = (contact) => contactApi.post('/', contact)
+    .then((response) => {
+        return {data:response, status:true, code:response.status}
+    })
+    .catch((error) => {
+        return {data:error, status:false, code:error.response.status}
+    });
--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -1,5 +1,39 @@
-import { ContactList } from "../components/ContactList"
+import {useForm} from 'react-hook-form'
+import { sendContact } from '../api/contact.api'
+import { useNavigate } from 'react-router-dom'
 
 export function ContactPage() {
-  return <div></div>
-}
+    const {register, handleSubmit, formState:{errors}} = useForm()
+
+    const navigate = useNavigate()
+
+    const onSubmit = handleSubmit(async data => {
+        const res = await sendContact(data)
+        if (res.status){
+            navigate('/contactlist')
+        }else{
+            console.log(res)
+        }
+    })
+    return <div>
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="Name" {...register("name", {required:true})}/>
+            {errors.name && <span className='error'>Name is required</span>}
+
+            <input type="email" placeholder="email@example.com" {...register("email", {
+                required:"Email is required", 
+                validate: {
+                    maxLength: (v) => v.length <= 50 
+                        || "The email should have at most 50 characters",
+                    matchPattern: (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v)
+                        || "Email address must be a valid address",
+                }
+            })}/>
+            {errors.email && <span className='error'>{errors.email.message}</span>}
+
+            <textarea rows="5" placeholder="Message" {...register("message", {required:true})}></textarea>
+            {errors.message && <span className='error'>Message is required</span>}
+            <button>Send</button>
+        </form>
+    </div>
+    }
