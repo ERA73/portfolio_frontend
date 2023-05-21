c8b54013fd24365e92c7663494c27e74de31e117
ERA73 <era-73@hotmail.com>
2023-05-21 13:31:54 -0500

    set directly localhost ip

--- client/src/api/contact.api.js
+++ client/src/api/contact.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const contactApi = axios.create({
-    baseURL: 'http://localhost:8000/contact/api/v1/contact/'
+    baseURL: 'http://127.0.0.1:8000/contact/api/v1/contact/'
 })
 
 // export const getAllContacts = () => {
