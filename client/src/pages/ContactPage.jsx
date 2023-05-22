3bf01e5d17a19a372cf81a1309c2f7a879d1e17f
ERA73 <era-73@hotmail.com>
2023-05-21 21:50:32 -0500

    Add function Contact

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -27,27 +27,38 @@ export function ContactPage() {
     })
     return (
         <div className='contact-container'>
-            <h2>Do you need more information?</h2>
-            <p>Let me know and I will answer you as soon as possible</p>
-            <form className='contact-form' onSubmit={onSubmit}>
-                <input className={errors.name && "error"} type="text" placeholder="Name" {...register("name", { required: true })} />
-                {/* {errors.name && <span className='error'>Name is required</span>} */}
-
-                <input className={errors.email && "error"} type="email" placeholder="email@example.com" {...register("email", {
-                    required: "Email is required",
-                    validate: {
-                        maxLength: (v) => v.length <= 50
-                            || "The email should have at most 50 characters",
-                        matchPattern: (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v)
-                            || "Email address must be a valid address",
-                    }
-                })} />
-                {/* {errors.email && <span className='error'>{errors.email.message}</span>} */}
-
-                <textarea className={errors.message && "error"} rows="5" placeholder="Message" {...register("message", { required: true })}></textarea>
-                {/* {errors.message && <span className='error'>Message is required</span>} */}
-                <button>Send</button>
-            </form>
+            <h2>
+                <span className="heading-main">Do you need more information?</span>
+                <span className="heading-sec">Let me know and I will answer you as soon as possible</span>
+            </h2>
+            <div className="contact-content">
+                <form className='contact-form' onSubmit={onSubmit}>
+                    <div className="contact-field">
+                        <label>Name</label>
+                        <input className={errors.name && "error"} type="text" placeholder="Enter Your Name" {...register("name", { required: true })} />
+                        {/* {errors.name && <span className='error'>Name is required</span>} */}
+                    </div>
+                    <div className="contact-field">
+                        <label>Email</label>
+                        <input className={errors.email && "error"} type="email" placeholder="e.g. email@example.com" {...register("email", {
+                            required: "Email is required",
+                            validate: {
+                                maxLength: (v) => v.length <= 50
+                                    || "The email should have at most 50 characters",
+                                matchPattern: (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v)
+                                    || "Email address must be a valid address",
+                            }
+                        })} />
+                        {/* {errors.email && <span className='error'>{errors.email.message}</span>} */}
+                    </div>
+                    <div className="contact-field-msg">
+                        <label>Message</label>
+                        <textarea className={errors.message && "error"} rows="5" placeholder="Enter Your Message" {...register("message", { required: true })}></textarea>
+                        {/* {errors.message && <span className='error'>Message is required</span>} */}
+                    </div>
+                    <button>Send</button>
+                </form>
+            </div>
             {status?.type === 'success' && <span className='success'>{status.data}</span>}
             {status?.type === 'error' && <span className='error'>{status.data}</span>}
         </div>
