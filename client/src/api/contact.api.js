4bc35270cb51e156937a3d49ee86eda8bac44403
ERA73 <era-73@hotmail.com>
2023-05-26 17:39:23 -0500

    change variable name

--- client/src/api/contact.api.js
+++ client/src/api/contact.api.js
@@ -1,29 +1,17 @@
 import axios from 'axios'
 
 const contactApi = axios.create({
-    // baseURL: 'http://127.0.0.1:8000/contact/api/v1/contact/'
-    baseURL: import.meta.env.REACT_APP_API_PORTFOLIO,
-    // buttom sections
+    baseURL: import.meta.env.VITE_API_PORTFOLIO,
     headers: {
         // 'Accept': 'application/json',
         'Content-Type': 'application/json;charset=UTF-8'
       },
-    // redirect: 'follow'
-    // top section
     
 })
 
-// export const getAllContacts = () => {
-//     return contactApi.get('/')
-// }
-// export const sendContact = (contact) => {
-//     return contactApi.post('/', contact)
-// }
-
-
 export const getAllContacts = () =>  contactApi.get('/');
 
-export const sendContact = (contact) => contactApi.post('/', contact)
+export const sendContact = (contact) => contactApi.post('/contact/api/v1/contact/', contact)
     .then(response => {
         console.log(response)
         return {data:response.data, status:true, code:response.status}
