b097dd72a5ac462a281b0a53215a5060c7813a2f
ERA73 <era-73@hotmail.com>
2025-05-04 19:31:46 -0500

    try redirect subdomain

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
-
+if (window.location.hostname.startsWith('www.')) {
+  const newHost = window.location.hostname.replace('www.', '');
+  const newUrl = `${window.location.protocol}//${newHost}${window.location.pathname}${window.location.search}`;
+  window.location.replace(newUrl);
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
